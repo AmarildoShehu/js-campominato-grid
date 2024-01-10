0f2aa01431f0dc5b9e6ba2855d85511fb0a02054
Amarildo <amarildos@outloo.it>
2024-01-10 16:45:42 +0100

    created style

--- js/script.js
+++ js/script.js
@@ -3,3 +3,11 @@ console.log('JS OK');
 // 1. Creare con js una griglia 10x10
 // 2. Ogni cella deve avere un numero progressivo da 1 a 100
 // 3. Quando il cliente clicca il pulsante plauìy appare la griglia
+
+//prendo l'elemento
+const grid = document.getElementById('grid');
+
+//creo costanti row e cols
+
+const row = 10;
+const cols= 10;
