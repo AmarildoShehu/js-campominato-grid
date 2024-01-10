503707cbf03ed41061efcfd145d1edf9dc194f14
Amarildo <amarildos@outloo.it>
2024-01-10 16:06:55 +0100

    added my first ladder

--- js/script.js
+++ js/script.js
@@ -1,1 +1,5 @@
 console.log('JS OK');
+
+// 1. Creare con js una griglia 10x10
+// 2. Ogni cella deve avere un numero progressivo da 1 a 100
+// 3. Quando il cliente clicca il pulsante plauìy appare la griglia
